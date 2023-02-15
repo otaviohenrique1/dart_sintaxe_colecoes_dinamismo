c9dc9d16aa2d7f5d70365761df2f823c3b3fd1b6
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-15 18:14:12 -0300

    commit 5

--- bin/dart_aula03.dart
+++ bin/dart_aula03.dart
@@ -1,4 +1,14 @@
 void testeDartAula03() {
+  /* var => tipo é atribuido na inicialização da variavel */
   var numero1 = 7.1;
+  print(numero1);
   print(numero1.runtimeType);
+
+  /* dynamic => tipo da variavel pode mudar em tempo de execução */
+  dynamic numero2 = 10;
+  print(numero2);
+  print(numero2.runtimeType);
+  numero2 = "texto";
+  print(numero2);
+  print(numero2.runtimeType);
 }
