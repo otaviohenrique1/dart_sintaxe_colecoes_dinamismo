30cdf70437d288d903c0cbe859e0c528dd7e790d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-15 11:28:37 -0300

    commit 2

--- bin/dart_sintaxe_colecoes_dinamismo.dart
+++ bin/dart_sintaxe_colecoes_dinamismo.dart
@@ -1,34 +1,57 @@
 void main(List<String> args) {
-  // print("Ola mundo");
-  // escolherMeioTransporte(0);
-  escolherMeioTransporteEnum(Transporte.carro);
+  escolherMeioTransporteEnumSwitch(Transporte.patins);
 }
 
-void escolherMeioTransporte(int locomocao) {
-  if (locomocao == 0) {
-    print("Carro");
-  } else if (locomocao == 1) {
-    print("Bike");
-  } else {
-    print("Vou para aventura!");
+enum Transporte {
+  carro,
+  bicicleta,
+  andando,
+  skate,
+  patins,
+  trem,
+}
+
+void escolherMeioTransporteEnumSwitch(Transporte locomocao) {
+  switch (locomocao) {
+    case Transporte.carro:
+      print("Carro");
+      break;
+    case Transporte.bicicleta:
+      print("Bicicleta");
+      break;
+    case Transporte.andando:
+      print("Andando");
+      break;
+    case Transporte.patins:
+      print("Patins");
+      break;
+    case Transporte.skate:
+      print("Skate");
+      break;
+    case Transporte.trem:
+      print("Trem");
+      break;
+    default:
+      print("Vou para aventura!");
   }
 }
 
 void escolherMeioTransporteEnum(Transporte locomocao) {
   if (locomocao == Transporte.carro) {
     print("Carro");
-  } else if (locomocao == Transporte.bike) {
-    print("Bike");
+  } else if (locomocao == Transporte.bicicleta) {
+    print("Bicicleta");
   } else {
     print("Vou para aventura!");
   }
 }
 
-enum Transporte {
-  carro,
-  bike,
-  andando,
-  skate,
-  patins,
-  trem,
+void escolherMeioTransporte(int locomocao) {
+  if (locomocao == 0) {
+    print("Carro");
+  } else if (locomocao == 1) {
+    print("Bicicleta");
+  } else {
+    print("Vou para aventura!");
+  }
 }
